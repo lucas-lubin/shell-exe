8b4c002f0ae6da2442d35df52868d861eb94a9f6
lucas-lubin <146708354+lucas-lubin@users.noreply.github.com>
2023-10-07 09:00:33 +0200

    Add files via upload

---
+++ job8/get_logs.sh
@@ -0,0 +1,8 @@
+
+file=number_connection-$(date +"%d-%m-%y-%R")
+user="vm"
+
+last | grep $user | wc -l > $file
+tar -cvf "/home/vm/Documents/shell-exe/Job08/$file.tar" "$file"
+mv "/home/vm/Documents/shell-exe/Job08/$file.tar" "/home/vm/Documents/shell-exe/Job08/Backup"
+rm "/home/vm/Documents/shell-exe/Job08/$file"
